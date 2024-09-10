09f4520e21d71c52147633160f9b802453c27d29
Antoine Grigis <antoine.grigis@cea.fr>
2024-09-10 09:26:57 +0200

    Get rid of "coding: utf-8"

--- caravel/loaders/_png.py
+++ caravel/loaders/_png.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 ##########################################################################
 # NSAp - Copyright (C) CEA, 2020
 # Distributed under the terms of the CeCILL-B license, as published by
