621ad0447a511c5f5e3d6cc08c220e9106bd7cb9
Neurospin-cloud admin <neurospin-cloud@cea.fr>
2024-11-29 14:19:36 +0100

    Clean log directories code

--- caravel/utils.py
+++ caravel/utils.py
@@ -12,6 +12,9 @@ This module provides common utilities.
 """
 
 # Imports
+import os
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
 import re
 from docx import Document
 
@@ -51,3 +54,58 @@ def export_report(report, timestamp, outfile):
                     cell = table.cell(idx, 1)
                     cell.text = val
     document.save(outfile)
+
+
+def get_logs_to_remove(log_dir, cut_date=None):
+    """ Return all files that are older than cut_date 
+    according to their name.
+    
+    Parameters
+    ----------
+    log_dir: str
+        path to the directory to clean.
+    cut_date: 
+        date from before which files are returned. If None,
+        remove files than are older than one year old.  
+        Should be formatted %Y-%m-%d (e.g. 2024-08-06).
+    """
+
+    print(f"Clean {log_dir}")
+    if cut_date is None:
+        current_date = date.today()
+        cut_date = current_date - relativedelta(years=1)
+    else:
+        cut_date = datetime.strptime(cut_date, "%Y-%m-%d").date()
+    print("Cut date", cut_date)
+    
+    files2remove = []
+
+    for filename in os.listdir(log_dir):
+        date_regex = r'20\d\d-(0|1)\d-(0|1|2|3)\d'
+        file_date = re.search(date_regex, filename)
+        if file_date is not None:
+            file_date = datetime.strptime(file_date.group(0), '%Y-%m-%d').date()
+            if file_date <= cut_date:
+                files2remove.append(filename)
+    
+    return files2remove
+
+
+def clean_logs_dir(log_dir, cut_date=None):
+    """ Remove all files that are older than cut_date.
+    
+    Parameters
+    ----------
+    log_dir: str
+        path to the directory to clean.
+    cut_date: 
+        date from before which files are suppressed. If None,
+        remove files than are older than one year old.  
+        Should be formatted %Y-%m-%d (e.g. 2024-08-06).
+    """
+
+    filenames = get_logs_to_remove(log_dir, cut_date=cut_date)
+    print(f"Number of files to remove: {len(filenames)}")
+    for filename in filenames:
+        filepath = os.path.join(log_dir, filename)
+        os.remove(filepath)
